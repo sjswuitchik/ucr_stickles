74a81bcada3b4ba81fc5ecd24c8b5af818dc628a
Sara J. Smith <ssmith6@mtroyal.ca>
2024-03-19 16:45:18 -0600

    Update 01_processing.sh

--- 01_processing.sh
+++ 01_processing.sh
@@ -2,4 +2,4 @@
 
 module load StdEnv/2020 stacks/2.64
 
-process_radtags -P -I -i gzfastq -1 NS.LH00147_0009.006.B723---B503.THigham_20230515_plate1_R1.fastq.gz -2 NS.LH00147_0009.006.B723---B503.THigham_20230515_plate1_R2.fastq.gz -o process_fastq -b barcodes.txt -e apeKI -r -c -q
+process_radtags -1 NS.LH00147_0009.006.B723---B503.THigham_20230515_plate1_R1.fastq.gz -2 NS.LH00147_0009.006.B723---B503.THigham_20230515_plate1_R2.fastq.gz -o process_fastq -b barcodes.txt -e apeKI -y fastq -r -c -q
