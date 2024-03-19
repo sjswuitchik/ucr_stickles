9c3a36d5b6b5644fce4d7ad81e7b44b0d456fccd
Sara J. Smith <ssmith6@mtroyal.ca>
2024-03-19 16:31:29 -0600

    Update 01_processing.sh

--- 01_processing.sh
+++ 01_processing.sh
@@ -1,3 +1,5 @@
 # in /home/sjsmith/projects/def-sjsmith/sjsmith/stickles_ucr/fastq_dl
 
 module load StdEnv/2020 stacks/2.64
+
+process_radtags -1 NS.LH00147_0009.006.B723---B503.THigham_20230515_plate1_R1.fastq.gz -2 NS.LH00147_0009.006.B723---B503.THigham_20230515_plate1_R2.fastq.gz -o ./process_fastq -b barcodes.txt -e apeKI -r -c -q
