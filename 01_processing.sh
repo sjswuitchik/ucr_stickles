959c2247cebe330b1727fc04b2bcdf0c6a4cb498
Sara J. Smith <ssmith6@mtroyal.ca>
2024-03-19 20:35:20 -0600

    Update 01_processing.sh

--- 01_processing.sh
+++ 01_processing.sh
@@ -29,6 +29,7 @@ multiqc 01_process_fastq/fastqc
 # remove PCR duplicates
 cd 01_process_fastq
 ls *.1.fq | sed '/\.1\.fq/s///' > samples
+
 while read file
 do
   clone_filter -1 $file.1.fq -2 $file.1.fq -i fastq -D -o filtered >> $file.log
@@ -37,6 +38,8 @@ done < samples
 cd ..
 
 # align to reference genome
+mkdir 02_align
+
 while read file
 do
   bwa mem -t 16 ../reference/GCF_016920845.1/GCF_016920845.1_GAculeatus_UGA_version5_genomic.fna 01_process_fastq/filtered/$file.1.1.fq 01_process_fastq/filtered/$file.1.2.fq > 02_align/$file.sam 2> 02_align/$file.bwa.log
