3efc93a786faa09c4c44921eebcab5c9182b413c
Sara J. Smith <ssmith6@mtroyal.ca>
2024-03-19 20:37:13 -0600

    Update 01_processing.sh

--- 01_processing.sh
+++ 01_processing.sh
@@ -6,10 +6,11 @@ conda create -n stacks -c bioconda stacks fastqc multiqc
 mkdir 01_process_fastq
 
 process_radtags -1 00_raw_fastq/NS.LH00147_0009.006.B723---B503.THigham_20230515_plate1_R1.fastq.gz -2 00_raw_fastq/NS.LH00147_0009.006.B723---B503.THigham_20230515_plate1_R2.fastq.gz -o 01_process_fastq -b barcodes.txt -e apeKI -y fastq -r -c -q
-#178395276 total sequences
-#    82910 barcode not found drops (0.0%)
-#   995028 low quality read drops (0.6%)
-#  1383657 RAD cutsite not found drops (0.8%)
+
+# 178395276 total sequences
+# 82910 barcode not found drops (0.0%)
+# 995028 low quality read drops (0.6%)
+# 1383657 RAD cutsite not found drops (0.8%)
 # 175933681 retained reads (98.6%)
 
 stacks-dist-extract --pretty 01_process_fastq/process_radtags.log per_barcode_raw_read_counts > 01_process_fastq/process.sum
