639c351d293372040c5c6240b8853dbb80e4814d
Sara J. Smith <ssmith6@mtroyal.ca>
2024-03-19 21:24:30 -0600

    Update 00_refDL.sh

--- 00_refDL.sh
+++ 00_refDL.sh
@@ -11,5 +11,6 @@ rm -r *.zip ncbi_dataset *.json*
 conda deactivate
 conda activate stacks
 conda install -c bioconda samtools=1.19.2 --force-reinstall # issue with libcrypto
+# conda install bioconda::samtools if the above continues to not work
 
 samtools faidx GCF_016920845.1_GAculeatus_UGA_version5_genomic.fna
