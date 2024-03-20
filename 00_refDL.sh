124bd6e86ba50ceb29509371a81a5fe2c3a62702
Sara J. Smith <ssmith6@mtroyal.ca>
2024-03-19 18:38:25 -0600

    Create 00_refDL.sh

---
+++ 00_refDL.sh
@@ -0,0 +1,6 @@
+# in projects/def-sjsmith/sjsmith/stickles_ucr/reference
+
+conda activate ncbi
+
+datasets download genome accession GCF_016920845.1 --include gff3,genome,seq-report
+unzip ncbi_dataset.zip
