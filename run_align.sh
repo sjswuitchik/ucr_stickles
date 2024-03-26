70a9499fe9c7ebe02841b77536587a784a9a2f31
Sara J. Smith <ssmith6@mtroyal.ca>
2024-03-26 11:00:37 -0600

    Update run_align.sh

--- run_align.sh
+++ run_align.sh
@@ -8,6 +8,8 @@
 
 # submit from ~/projects/def-sjsmith/sjsmith/stickles_ucr/seq_data
 
+conda activate stacks
+
 while read file
 do
   bwa mem -t 16 ../reference/GCF_016920845.1/GCF_016920845.1_GAculeatus_UGA_version5_genomic.fna 01_process_fastq/filtered/$file.1.1.fq 01_process_fastq/filtered/$file.1.2.fq > 02_align/$file.sam 2> 02_align/$file.bwa.log
