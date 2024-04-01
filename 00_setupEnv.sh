e47396569f42fd616c43b5edf4be8f5cd4712e5f
Sara J. Smith <ssmith6@mtroyal.ca>
2024-04-01 17:30:37 -0600

    Update 00_setupEnv.sh

--- 00_setupEnv.sh
+++ 00_setupEnv.sh
@@ -1,6 +1,3 @@
 conda create -n fastq2vcf -c bioconda stacks fastqc multiqc bwa bcftools samtools fastp sambamba java-jdk
+conda activate fastq2vcf
 conda install -c bioconda openssl=1.0 # for bcftools issue
-
-conda create -n ddocent -c bioconda ddocent # in case I decide to use dDocent
-
-conda create -n gatk -c bioconda gatk4 picard sambamba java-jdk
