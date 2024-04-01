484f58efd8c0bcbe5d7ae2cf0fb6fe6309ca5602
Sara J. Smith <ssmith6@mtroyal.ca>
2024-04-01 11:19:28 -0600

    Create run_align_v2.sh

---
+++ run_align_v2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J align
+#SBATCH -o 02_align/out
+#SBATCH -e 02_align/err
+#SBATCH -n 1
+#SBATCH --mem 10000
+#SBATCH -t 24:00:00
+
+# submit from ~/projects/def-sjsmith/sjsmith/stickles_ucr/seq_data/runv2
+
+module load bwa
+
+while read file
+do
+  bwa mem -t 16 ../../reference/GCF_016920845.1/GCF_016920845.1_GAculeatus_UGA_version5_genomic.fna 01_demulti/filtered/$file.1.1.fq 01_demulti/filtered/$file.1.2.fq > 02_align/$file.sam 2> 02_align/$file.bwa.log
+done < 01_demulti/samples
