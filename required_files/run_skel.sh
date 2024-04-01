81bdb72e396077d94a261b485c903f81eb136ec9
Sara J. Smith <ssmith6@mtroyal.ca>
2024-04-01 17:36:30 -0600

    Update and rename run_align_v2.sh to run_skel.sh

---
+++ required_files/run_skel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J job
+#SBATCH -o out
+#SBATCH -e err
+#SBATCH -n 1
+#SBATCH --mem 10000
+#SBATCH -t 24:00:00
+
+# submit from ~/projects/def-sjsmith/sjsmith/stickles_ucr/seq_data/
