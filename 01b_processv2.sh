c977a4cc0057cfea8321d70840d531595e098ba9
Sara J. Smith <ssmith6@mtroyal.ca>
2024-04-01 14:23:07 -0600

    Update 01b_processv2.sh

--- 01b_processv2.sh
+++ 01b_processv2.sh
@@ -30,7 +30,6 @@ do
   fastp --in1 $file.1.fq --in2 $file.2.fq --out1 trimmed/$file.R1.fq.gz --out2 trimmed/$file.R2.fq.gz -q 15 -u 50 -t 1 -T 1 -c -z 4 --dedup -h fastP_out/$file.fp.html
 done < samples
 
-## NTS: double check these names after fastp is done
 while read file
 do
   clone_filter -1 trimmed/$file.R1.fq.gz -2 trimmed/$file.R2.fq.gz -i gzfastq -D -o trimmed/filtered >> $file.log
