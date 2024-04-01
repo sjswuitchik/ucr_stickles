f7d423002fc7f47f668e47a3794bbe516a43d317
Sara J. Smith <ssmith6@mtroyal.ca>
2024-04-01 16:47:58 -0600

    Update 00_setupEnv.sh

--- 00_setupEnv.sh
+++ 00_setupEnv.sh
@@ -3,4 +3,4 @@ conda install -c bioconda openssl=1.0 # for bcftools issue
 
 conda create -n ddocent -c bioconda ddocent # in case I decide to use dDocent
 
-conda create -n gatk -c bioconda gatk4 picard sambamba
+conda create -n gatk -c bioconda gatk4 picard sambamba java-jdk
