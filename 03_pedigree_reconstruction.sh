5de9ea789dfe44bcfb52a9fab6fbd39fc10232bf
Sara J. Smith <ssmith6@mtroyal.ca>
2024-04-16 16:25:45 -0600

    Update 03_pedigree_reconstruction.sh

--- 03_pedigree_reconstruction.sh
+++ 03_pedigree_reconstruction.sh
@@ -1,4 +1,11 @@
 ## in /home/sjsmith/projects/def-sjsmith/sjsmith/stickles_ucr/vcf_filt
-# transfer 012 matrix data to local machine to create input files for R/sequoia
-# local wd: ~/Desktop/MRU_Faculty/Research/ucr_stickles/012_matrix
+# transfer 012 matrix indv to local machine to add 0-59 as first field then back to Cedar
 
+conda activate rv4
+conda update -c conda-forge r-base
+
+R
+library(tidyverse)
+df <- read_delim("stickles.filtered.012", delim = '\t', col_names = "num")
+indv <- read_delim("stickles.filtered.012.indv.join", delim = '\t', col_names = c("num", "id"))
+clean <- left_join(df, indv, by = 'num')
