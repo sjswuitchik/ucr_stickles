8806d9e0a9b61e17aea57d8c0d7d54ab4defae01
Sara J. Smith <ssmith6@mtroyal.ca>
2024-04-16 11:50:55 -0600

    Update 02_linkageMap.sh

--- 02_linkageMap.sh
+++ 02_linkageMap.sh
@@ -18,5 +18,9 @@ mkdir -p snpEff/data/gasAcu
 
 # create a PLINK env to generate IBD stats for manaul clustering 
 conda create -n plink -c bioconda plink
+conda activate plink
+
+cp seq_data/02_align/dedup/03_vcf/stickles_ucr.dedup.vcf linkageMapping/
+
 
 
