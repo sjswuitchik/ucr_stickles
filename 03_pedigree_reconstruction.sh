c18e90dd816dc4f5781f775f78612183dbbcf84e
Sara J. Smith <ssmith6@mtroyal.ca>
2024-04-16 12:56:04 -0600

    Update 03_pedigree_reconstruction.sh

--- 03_pedigree_reconstruction.sh
+++ 03_pedigree_reconstruction.sh
@@ -1,1 +1,4 @@
+## in /home/sjsmith/projects/def-sjsmith/sjsmith/stickles_ucr/vcf_filt
+# transfer 012 matrix data to local machine to create input files for R/sequoia
+# local wd: ~/Desktop/MRU_Faculty/Research/ucr_stickles/012_matrix
 
