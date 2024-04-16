c3b54b0c6555232d1878f053409974c1b0362dd6
Sara J. Smith <ssmith6@mtroyal.ca>
2024-04-16 12:23:01 -0600

    Update 02_qcFiltering.sh

--- 02_qcFiltering.sh
+++ 02_qcFiltering.sh
@@ -2,3 +2,29 @@
 
 # conda create -n vcfFilt -c bioconda plink vcftools htslib bcftools rename
 conda activate vcfFilt
+
+mkdir vcf_filt
+cp seq_data/02_align/dedup/03_vcf/stickles_ucr.dedup.vcf vcf_filt
+cd vcf_filt
+
+bgzip stickles_ucr.dedup.vcf -c > stickles_ucr.dedup.vcf.gz
+tabix -p vcf stickles_ucr.dedup.vcf.gz
+
+vcftools --gzvcf stickles_ucr.dedup.vcf.gz --remove-indels --max-missing 0.5 --mac 3 --minQ 30 --recode --recode-INFO-all --out stickles.filt1
+# kept 470374/944649
+vcftools --vcf stickles.filt1.recode.vcf --minDP 3 --recode --recode-INFO-all --out stickles.filt2
+# kept 470374/470374
+vcftools --vcf stickles.filt2.recode.vcf --missing-indv
+# no one to remove
+vcftools --vcf stickles.filt2.recode.vcf --remove-indels --min-alleles 2 --max-alleles 2 --max-missing 0.75 --minDP 3 --recode --recode-INFO-all --out stickles.filt3
+# kept 470374/470374
+vcftools --vcf stickles.filt3.recode.vcf --maf 0.01 --max-meanDP 200 --recode --recode-INFO-all --out stickles.filtered
+#final checks; kept 470374/470374 sites, 60/60 indv
+
+mkdir int_filt
+mv out.* stickles.filt* int_filt
+tar zcvf int_filt.tar.gz int_filt/
+rm -r int_filt/
+
+bgzip stickles.filtered.recode.vcf -c > stickles.filt.vcf.gz
+tabix -p vcf stickles.filt.vcf.gz
