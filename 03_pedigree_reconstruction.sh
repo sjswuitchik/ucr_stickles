fc9bdd8e3aa3d078dbe1b3328b7c6871f118035d
Sara J. Smith <ssmith6@mtroyal.ca>
2024-04-19 14:27:26 -0600

    Update 03_pedigree_reconstruction.sh

--- 03_pedigree_reconstruction.sh
+++ 03_pedigree_reconstruction.sh
@@ -1,5 +1,5 @@
 ## in /home/sjsmith/projects/def-sjsmith/sjsmith/stickles_ucr/vcf_filt
-# transfer 012 matrix indv to local machine to add 0-59 as first field, and replace -1 in 012 matrix to -9 for sequoia
+# transfer 012 matrix indv to local machine to add 0-59 as first field
 
 ## if doing on Cedar 
 #conda create -n rv4 -c conda-forge r-base=4.3.3
@@ -11,4 +11,6 @@ library(tidyverse)
 
 df <- read_delim("stickles.filtered.012", delim = '\t', col_names = "num")
 indv <- read_delim("stickles.filtered.012.indv.join", delim = '\t', col_names = c("num", "id"))
-clean <- left_join(df, indv, by = 'num')
+clean <- left_join(indv, df, by = 'num') %>%
+  select(-c(num))
+clean[ clean == -1 ] <- -9
