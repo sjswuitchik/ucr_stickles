e4f557b6adf4dbc564d8167224f20c84909df3ab
Sara J. Smith <ssmith6@mtroyal.ca>
2024-04-19 14:05:10 -0600

    Update 03_pedigree_reconstruction.R

--- 03_pedigree_reconstruction.sh
+++ 03_pedigree_reconstruction.sh
@@ -1,12 +1,13 @@
 ## in /home/sjsmith/projects/def-sjsmith/sjsmith/stickles_ucr/vcf_filt
-# transfer 012 matrix indv to local machine to add 0-59 as first field then back to Cedar
+# transfer 012 matrix indv to local machine to add 0-59 as first field
 
-conda create -n rv4 -c conda-forge r-base=4.3.3
-conda activate rv4
+## if doing on Cedar 
+#conda create -n rv4 -c conda-forge r-base=4.3.3
+#conda activate rv4
+#R
 
-R
-install.packages('tidyverse')
 library(tidyverse)
+
 df <- read_delim("stickles.filtered.012", delim = '\t', col_names = "num")
 indv <- read_delim("stickles.filtered.012.indv.join", delim = '\t', col_names = c("num", "id"))
 clean <- left_join(df, indv, by = 'num')
