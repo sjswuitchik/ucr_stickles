3d73da33932219c8f15db7278d25fd2407298563
Sara J. Smith <ssmith6@mtroyal.ca>
2024-05-07 12:43:26 -0600

    Update 03_pedigree_reconstruction.R

--- 03_pedigree_reconstruction.R
+++ 03_pedigree_reconstruction.R
@@ -1,7 +1,11 @@
 ## in /home/sjsmith/projects/def-sjsmith/sjsmith/stickles_ucr/vcf_filt
-# transfer 012 matrix indv to local machine to add 0-59 as first field - saved as stickles.filtered.012.indv.join
 
-## if doing on Cedar 
+conda activate vcfFilt
+
+vcftools --vcf stickles.filtered.recode.vcf --012 --out stickles.filtered
+# transfer 012 files to local machine to add 0-59 as first field of indv - saved as stickles.filtered.012.indv.join
+
+####### if doing on Cedar 
 #conda create -n rv4 -c conda-forge r-base=4.3.3
 #conda activate rv4
 #R
@@ -15,11 +19,33 @@ indv <- read_delim("~/Desktop/MRU_Faculty/Research/ucr_stickles/012_matrix/stick
 ## write out 012 matrix for SNPrune
 prune <- left_join(indv, df, by = 'num') %>%
   select(-c(num)) %>%
-  filter(id != 'dedup/BAM_11.dedup.bam') %>%
-  filter(id != 'dedup/OBBB_1.dedup.bam') %>%
-  filter(id != 'dedup/OOB_1.dedup.bam')
+  filter(id != 'dedup/BAM_11.dedup.bam') %>% #low mappability
+  filter(id != 'dedup/OBBB_1.dedup.bam') %>% # f1
+  filter(id != 'dedup/OOB_1.dedup.bam')      # f1
 
 # replace -1 from vcftools with -9 
 prune[ prune == -1 ] <- -9
 write_delim(prune, "~/Desktop/stickles.012matrix.txt", delim = ' ', )
+
+####
+# sftp stickles.012matrix.txt to Cedar in /home/sjsmith/projects/def-sjsmith/sjsmith/stickles_ucr/vcf_filt
+
+git clone https://github.com/mariocalus/SNPrune.git
+cd SNPrune
+chmod +x SNPrune
+./SNPrune
+
+
+
+##############################
+
+## calculate IBD in PLINK for pedigree determination
+#plink --vcf stickles.filtered.recode.vcf --allow-extra-chr --recode --out test
+#plink --file test --allow-extra-chr --genome --out test2
+
+#plink --vcf stickles.filtered.recode.vcf --allow-extra-chr --set-all-var-ids --recode --out stickles.plink
+#plink --file stickles.plink --allow-extra-chr --indep-pairwise 100000 50000 0.5 --out stickles.plink
+
+
+
 
