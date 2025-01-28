9187ea4de470c63001287a33db91e41db9d38794
Sara J. Smith <ssmith6@mtroyal.ca>
2025-01-28 12:22:07 -0700

    Create 03_gwas.sh

---
+++ 03_gwas.sh
@@ -0,0 +1,15 @@
+# create env for gemma gwas
+conda conda create -n gwas -c esgf -c bioconda -c ostrokach python plink vcftools bcftools gemma
+
+# get gemma
+git clone https://github.com/johanzi/gwas_gemma.git
+
+## chromosomes need to be converted from alpahnumeric to numeric values. 
+# create chromosome map
+bcftools view -H stickles.filtered.recode.vcf | cut -f 1 | uniq | awk '{print $0"\t"$0}' > gasAcu.chrom.map.txt
+
+## edit in R (because I don't have the capacity to loop this rn) gasAcu.chrom.map.txt using sequence report from NCBI (https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_016920845.1/) to assign numerical chr names, then save as gasAcu.chromMap
+
+
+
+bcftools annotate stickles.filtered.recode.vcf --rename-chrs oncNer.chromMap -o oncNer.filter.chrRename.vcf -O v
