7b4982151a434868cacaa1a69d4e971be845edbd
Sara J. Smith <ssmith6@mtroyal.ca>
2025-05-06 14:51:31 -0600

    Update 06_LDmatrices.sh

--- 06_LDmatrices.sh
+++ 06_LDmatrices.sh
@@ -2,3 +2,5 @@ conda activate plink2
 
 plink --bfile gasAcu.plink19.maxCranElev --extract mce_sig_snps.txt --r2 square --allow-extra-chr --out mce_ld_matrix
 plink --bfile gasAcu.plink19.maxDecel --extract md_sig_snps.txt --r2 square --allow-extra-chr --out md_ld_matrix
+plink --bfile gasAcu.plink19.time_HDvMG --extract thdvmg_sig_snps.txt --r2 square --allow-extra-chr --out thdvmg_ld_matrix
+plink --bfile gasAcu.plink19.ttpg --extract ttpg_sig_snps.txt --r2 square --allow-extra-chr --out ttpg_ld_matrix
