e427281f3fdd6191605cb0aaa22d60d9a0721cd9
Sara J. Smith <ssmith6@mtroyal.ca>
2025-05-06 14:05:02 -0600

    Update 04_manhattans.R

--- 04_manhattans.R
+++ 04_manhattans.R
@@ -1,9 +1,11 @@
-# libraries & set up 
 library(qqman)
-library(kableExtra)
 library(tidyverse)
 
-# reading in all continuous GWAS results, cleaning, and saving as unique objects
+
+# read in each continuous GWAS result & clean
+
+setwd("~/Desktop/MRU_Faculty/Research/ucr_stickles/gwas_results")
+
 file_dir <- "~/Desktop/MRU_Faculty/Research/ucr_stickles/gwas_results"
 file_paths <- list.files(file_dir, pattern="gasAcu.plink.*.glm.linear", full.names = TRUE)
 
@@ -12,83 +14,69 @@ for (file in file_paths) {
   
   name <- str_split(basename(file), "\\.", simplify = TRUE)[, 3]
   
-  data <- read_table(file) %>%
-    rename(CHR = "#CHROM") %>%
-    select(CHR, POS, P) %>%
+  data <- read.table(file, col.names = c("CHR",	"POS", "ID", "REF",	"ALT", "A1",	"TEST",	"OBS_CT",	"OR",	"LOG(OR)_SE",	"Z_STAT",	"P")) %>%
+    dplyr::select(CHR, POS, ID, P) %>%
     filter((CHR <= 22 | is.na(CHR))) %>%
-    mutate(snp = c(1:15861)) %>%
     filter(CHR != 1.1 | is.na(CHR)) %>%
     filter(CHR != 21.1 | is.na(CHR)) %>%
-    mutate(CHR = replace_na(CHR, 23)) %>%
-    rename(chr = CHR, pos = POS, p = P, SNP = snp)
+    mutate(CHR = replace_na(CHR, 23))
   
-  assign(name, data, envir = .GlobalEnv)  
+  assign(name, data, envir = .GlobalEnv)
 }
 
-# reading in binary GWAS results & cleaning
-sf <- read_table("gwas_results/gasAcu.plink.sf.glm.logistic") %>%
-  rename(CHR = "#CHROM") %>%
-  select(CHR, POS, P) %>%
+# read in binary GWAS result
+
+sf <- read.table("gasAcu.plink.sf.glm.logistic", col.names = c("CHR",	"POS", "ID", "REF",	"ALT", "A1",	"TEST",	"OBS_CT",	"OR",	"LOG(OR)_SE",	"Z_STAT",	"P")) %>%
+  dplyr::select(CHR, POS, ID, P) %>%
   filter((CHR <= 22 | is.na(CHR))) %>%
-  mutate(snp = c(1:15861)) %>%
   filter(CHR != 1.1 | is.na(CHR)) %>%
   filter(CHR != 21.1 | is.na(CHR)) %>%
-  mutate(CHR = replace_na(CHR, 23)) %>%
-  rename(chr = CHR, pos = POS, p = P, SNP = snp)
+  mutate(CHR = replace_na(CHR, 23))
 
-# Manhattan plots & tables of SNPs with genome-wide significance (where relevant)
+
+# make manhattan plots for each phenotype & pull SNP IDs with genome-wide significance
+## NB: blue (suggestive) line -log10(1E-5), red (genome-wide) line -log10(5E-8)
+
 threshold <- 5E-8
 
-manhattan(dist, main = "Dist", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no genome-wide sig (GWS)
+manhattan(dist, main = "Dist", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no genome-wide sig (GWS)
 
-manhattan(maxCranElev, main = "Max Cran Elev", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # genome wide sig
+manhattan(maxCranElev, main = "Max Cran Elev", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # genome wide sig
 
-maxCranElev %>%
-  filter(p < threshold) %>%
-  kbl() %>%
-  kable_minimal()
+mce_sig <- maxCranElev %>%
+  filter(P < threshold)
 
-manhattan(maxDecel, main = "Max Decel", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # GWS
+manhattan(maxDecel, main = "Max Decel", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # GWS
 
-maxDecel %>%
-  filter(p < threshold) %>%
-  kbl() %>%
-  kable_minimal()
+md_sig <- maxDecel %>%
+  filter(P < threshold)
 
-manhattan(maxGape, main = "Max Gape", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no GWS
+manhattan(maxGape, main = "Max Gape", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no GWS
 
-manhattan(maxHD, main = "Max HD", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no GWS
+manhattan(maxHD, main = "Max HD", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no GWS
 
-manhattan(maxJP, main = "Max JP", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no GWS
+manhattan(maxJP, main = "Max JP", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no GWS
 
-manhattan(PPD_MG, main = "PPD_MG", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # GWS
+manhattan(PPD_MG, main = "PPD_MG", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # GWS
 
-PPD_MG %>%
-  filter(p < threshold) %>%
-  kbl() %>%
-  kable_minimal()
+ppdmg_sig <- PPD_MG %>%
+  filter(P < threshold)
 
-manhattan(PPD_SI, main = "PPD SI", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no GWS
+manhattan(PPD_SI, main = "PPD SI", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no GWS
 
-manhattan(ramSpeed, main = "Ram Speed", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # GWS
+manhattan(ramSpeed, main = "Ram Speed", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # GWS
 
-ramSpeed %>%
-  filter(p < threshold) %>%
-  kbl() %>%
-  kable_minimal()
+rs_sig <- ramSpeed %>%
+  filter(P < threshold)
 
-manhattan(sf, main = "Success/Failure", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no GWS
+manhattan(sf, main = "Success/Failure", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # no GWS
 
-manhattan(time_HDvMG, main = "Time - HD v MG", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # GWS
+manhattan(time_HDvMG, main = "Time - HD v MG", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # GWS
 
-time_HDvMG %>%
-  filter(p < threshold) %>%
-  kbl() %>%
-  kable_minimal()
+thdvmg_sig <- time_HDvMG %>%
+  filter(P < threshold)
 
-manhattan(ttpg, main = "TTPG", chr = "chr", bp = "pos", p = "p", snp = "SNP", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # GWS
+manhattan(ttpg, main = "TTPG", chr = "CHR", bp = "POS", p = "P", snp = "ID", ylim = c(0, 10), chrlabs = c(1:21, "Y", "MT"), col = c("skyblue", "grey")) # GWS
 
-ttpg %>%
-  filter(p < threshold) %>%
-  kbl() %>%
-  kable_minimal()
+ttpg_sig <- ttpg %>%
+  filter(P < threshold)
