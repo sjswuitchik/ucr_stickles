018e67e213e203f92831b7f2e77c90d2afd1deb6
Sara J. Smith <ssmith6@mtroyal.ca>
2025-05-07 12:59:12 -0600

    Update 08_geneSNPintersect.sh

--- 08_geneSNPintersect.sh
+++ 08_geneSNPintersect.sh
@@ -7,8 +7,13 @@ convert2bed --input=gff --output=bed < gasAcu.gff > gasAcu.sorted.bed
 
 # GFF has whitespaces in the notes, which doesn't work with current version of replace_chrs.pl and I don't have time to rewrite that, so we're doing this instead
 join gasAcu_acckey gasAcu.sorted.bed > gasAcu.sorted.repl.int.bed
-awk -F" " 'BEGIN{OFS=" "}{for(i=2;i<NF;i++){printf "%s%s",$i,OFS} print $NF}' gasAcu.sorted.repl.int.bed > gasAcu.sorted.repl.bed
+awk -F" " 'BEGIN{OFS=" "}{for(i=2;i<NF;i++){printf "%s%s",$i,OFS} print $NF} END{OFS="\t"}' gasAcu.sorted.repl.int.bed > gasAcu.sorted.repl.bed
+######## this didn't work, try after conference
 
+mkdir ../../geneAssoc
+mv gasAcu.sorted.* ../../geneAssoc
+cd ../../geneAssoc
 
+bedtools sort mce_sig_forBED.bed | bedtools intersect -a - -b gasAcu.sorted.repl.bed | less
 
 
