c7a8c1cdec3114ee8b8a47a88beda10e75f479d8
Sara J. Smith <ssmith6@mtroyal.ca>
2025-05-07 10:15:09 -0600

    Update 08_geneSNPintersect.sh

--- 08_geneSNPintersect.sh
+++ 08_geneSNPintersect.sh
@@ -1,6 +1,7 @@
 # in ~/projects/def-sjsmith/sjsmith/stickles_ucr/reference/GCF_016920845.1
 
 conda activate bedtools 
+module load perl # some conda issues with CC
 
 gff2bed < gasAcu.gff > gasAcu_sorted.bed
 
