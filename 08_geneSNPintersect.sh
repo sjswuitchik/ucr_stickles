753e9606bbab98f209ac8c2dda6afff916e4352f
Sara J. Smith <ssmith6@mtroyal.ca>
2025-05-07 12:01:09 -0600

    Update 08_geneSNPintersect.sh

--- 08_geneSNPintersect.sh
+++ 08_geneSNPintersect.sh
@@ -4,8 +4,6 @@ conda activate bedtools
 module load perl # some conda issues with CC
 
 chmod +x replace_chrs.pl
-./replace_chrs.pl gasAcu_acckey gasAcu.gff > gasAcu.repl.gff
-# manually add gff3 header back into gasAcu.repl.gff because I don't have time to re-write replace_chrs.pl right now
 
-convert2bed --input=gff --output=bed < gasAcu.repl.gff > gasAcu_sorted.bed
+convert2bed --input=gff --output=bed < gasAcu.gff > gasAcu.sorted.bed
 
