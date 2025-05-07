88fb6389310e72571b092aaf7e07df1afaf6f369
Sara J. Smith <ssmith6@mtroyal.ca>
2025-05-07 09:22:45 -0600

    Update 00_setupEnv.sh

--- 00_setupEnv.sh
+++ 00_setupEnv.sh
@@ -1,3 +1,6 @@
+# different conda envs used in different scripts
 conda create -n fastq2vcf -c bioconda stacks fastqc multiqc bwa bcftools samtools fastp sambamba java-jdk
 conda activate fastq2vcf
 conda install -c bioconda openssl=1.0 # for bcftools issue
+
+conda create -n vcfFilt -c bioconda plink vcftools htslib bcftools rename
