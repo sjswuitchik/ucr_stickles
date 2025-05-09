2ebce2620d92010454250e0abbdf7ce8db03fa8c
Sara J. Smith <ssmith6@mtroyal.ca>
2025-05-09 12:18:21 -0600

    Update 08_geneSNPintersect.sh

--- 08_geneSNPintersect.sh
+++ 08_geneSNPintersect.sh
@@ -26,11 +26,21 @@ chmod +x ./twoBitInfo
  ./faToTwoBit ../reference/GCF_016920845.1/GCF_016920845.1_GAculeatus_UGA_version5_genomic.fna gasAcu.fa.2bit
  ./twoBitInfo gasAcu.fa.2bit stdout | sort -k2rn > gasAcu.chrom.sizes
 
-bedtools makewindows -g gasAcu.chrom.sizes -w 100000 -s 50000 > gasAcu.windows.bed
-bedtools slop -i gasAcu.gene.bed -g gasAcu.chrom.sizes -b 100000 | less -S 
+bedtools slop -i gasAcu.gene.bed -g gasAcu.chrom.sizes -b 100000 | cut -f1,2,3,10 > gasAcu.windows.bed
+
+bedtools merge -i gasAcu.windows.bed -d -1 -c 4 -o distinct | bedtools sort -i - | less -S  
 
 ##### this needs to be dealt with tomorrow to make sure the SNP is being captured within the window
-##### intersect, merge, perhaps slop? 
+##### intersect, merge, perhaps slop?
+### some more dev: 
+awk '$3 == "CDS"' gasAcu.gff > test.onlyCDS.gff
+awk -f gff2bed.awk test.onlyCDS.gff > test.onlyCDS.bed 
+cat test.onlyCDS.bed | python genenames.py > test.onlyCDS.genes.bed
+bedtools slop -i test.onlyCDS.genes.bed -g gasAcu.chrom.sizes -b 100000 | bedtools sort -i - > test.windows.bed
+sed 's/NA/MT/g' gasAcu_acckey > test.acckey
+./replace_chrs.pl test.acckey test.windows.bed > test.windows.repl.bed
+
+##### the stuff between the hashes works, clean this up post-conference 
 
 bedtools sort -i mce_sig_forBED.bed | bedtools intersect -a - -b gasAcu.sorted.bed -wb > mce_windows.bed
 bedtools sort -i md_sig_forBED.bed | bedtools intersect -a - -b gasAcu.sorted.bed -wb > md_windows.bed
