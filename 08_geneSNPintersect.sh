c61327ca30cd500a086cc362bbe766632c2f7d88
Sara J. Smith <ssmith6@mtroyal.ca>
2025-05-08 18:03:00 -0600

    Update 08_geneSNPintersect.sh

--- 08_geneSNPintersect.sh
+++ 08_geneSNPintersect.sh
@@ -16,6 +16,7 @@ sed 's/ //g' gasAcu.bed > gasAcu.nws.bed
 bedtools sort -i gasAcu.nws.repl.bed > gasAcu.sorted.bed
 
 ##### this needs to be dealt with tomorrow to make sure the SNP is being captured within the window
+##### intersect, merge, perhaps slop? 
 
 bedtools sort -i mce_sig_forBED.bed | bedtools intersect -a - -b gasAcu.sorted.bed -wb > mce_windows.bed
 bedtools sort -i md_sig_forBED.bed | bedtools intersect -a - -b gasAcu.sorted.bed -wb > md_windows.bed
