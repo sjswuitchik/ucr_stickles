b4e3cc101aba2b6d245934061b6ea443598e7bc6
Sara J. Smith <ssmith6@mtroyal.ca>
2025-05-26 10:41:24 -0400

    Update 10_snpGeneIntersect_test.sh

--- 10_snpGeneIntersect_test.sh
+++ 10_snpGeneIntersect_test.sh
@@ -2,3 +2,6 @@
 conda activate vcfFilt
 
 vcftools --vcf ../gasAcu.filter.chrRename.vcf --bed cleanGenes.bed --recode --recode-INFO-all --out gasAcu.snps
+
+# took this output to use in a trial of OmicsBox to run Genetic Variation > Variant Annotation 
+# using GO version March 16, 2025, with reference genome and annotation from GAculeatus_UGA_version5 (https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_016920845.1/) 
