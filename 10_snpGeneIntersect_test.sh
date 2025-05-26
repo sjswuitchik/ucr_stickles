9c2a17422ec13563e4d0a64b269d20bef2d5eada
Sara J. Smith <ssmith6@mtroyal.ca>
2025-05-26 10:51:42 -0400

    Update 10_snpGeneIntersect_test.sh

--- 10_snpGeneIntersect_test.sh
+++ 10_snpGeneIntersect_test.sh
@@ -10,5 +10,5 @@ cat gasAcu.chromMap | awk '{print $2 "\t" $1}' > gasAcu.revChrMap
 bcftools annotate gasAcu.snps.recode.vcf --rename-chrs gasAcu.revChrMap -o gasAcu.snps.chrRename.vcf -O v
 
 
-# took this output to use in a trial of OmicsBox to run Genetic Variation > Variant Annotation 
+# took this output to use in a trial of OmicsBox v.3.4.6 to run Genetic Variation > Variant Annotation 
 # using GO version March 16, 2025, with reference genome and annotation from GAculeatus_UGA_version5 (https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_016920845.1/) 
