22509e9adc90c7dbd3a5876ce1326c410e191346
Sara J. Smith <ssmith6@mtroyal.ca>
2025-08-15 14:31:07 -0600

    Update 05_LD_geneAssoc_prep.R

--- 05_LD_geneAssoc_prep.R
+++ 05_LD_geneAssoc_prep.R
@@ -88,3 +88,51 @@ ttpg_bed <- ttpg.sig %>%
 
 clean_bed <- bind_rows(mce_bed, md_bed, thdvmg_bed, tmdvmg_bed, ttpg_bed)
 write_delim(clean_bed, "geneAssoc/cleanGenes.bed", delim = '\t', col_names = F)
+
+##########################
+## Morphological Traits ##
+##########################
+
+ray.pvar <- read.table("plink_out/gasAcu.plink.ray.pvar", 
+                 header = F, 
+                 col.names = c("CHR", "POS", "ID", "REF", "ALT", "QUAL", "INFO")) %>%
+  filter((CHR <= 22 | is.na(CHR))) %>%
+  filter(CHR != 1.1 | is.na(CHR)) %>%
+  filter(CHR != 21.1 | is.na(CHR)) %>%
+  mutate(CHR = replace_na(CHR, 23)) %>%
+  dplyr::select(-c(QUAL, INFO)) %>%
+  rename(chrom = CHR, pos = POS, variant.id = ID,)
+
+ray.sig.ann <- ray.sig %>% 
+  inner_join(ray.pvar, by = c("chrom", "pos", "variant.id")) %>%
+  dplyr::select(variant.id) %>%
+  write_delim(., "gwas_out/ray_sig_snps.txt", delim = '\t')
+
+# LD matrix
+ray.ld.mat <- read.table("gwas_out/ray_ld_matrix.ld", header = F)
+rownames(ray.ld.mat) <- ray.sig.ann$variant.id
+colnames(ray.ld.mat) <- ray.sig.ann$variant.id
+
+ray.ld.long <- melt(as.matrix(ray.ld.mat), varnames = c("SNP_A", "SNP_B"), value.name = "R2")
+
+ggplot(ray.ld.long, aes(x = SNP_A, y = SNP_B, fill = R2)) +
+  geom_tile() +
+  scale_fill_viridis(option = "viridis") + 
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 90, vjust = 0.5, size = 6),
+    axis.text.y = element_text(size = 6),
+    panel.grid = element_blank()
+  ) +
+  labs(title = "LD Heatmap (r²) Ray Count", x = "SNP", y = "SNP", fill = expression(r^2))
+
+ray_bed <- ray.sig %>% 
+  inner_join(ray.pvar, by = c("chrom", "pos", "variant.id")) %>%
+  select(-c(REF, ALT, p)) %>%
+  rename(end = pos) %>%
+  mutate(start = end - 1,
+         pheno = "ray") %>%
+  select(chrom, start, end, variant.id, pheno)
+
+write_delim(ray_bed, "gwas_out/rayGenes.bed", delim = '\t', col_names = F)
+
