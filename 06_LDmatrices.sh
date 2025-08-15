27837b16155f2ca7c7562eb7dc1be9dde92e40ec
Sara J. Smith <ssmith6@mtroyal.ca>
2025-08-15 14:20:36 -0600

    Update 06_LDmatrices.sh

--- 06_LDmatrices.sh
+++ 06_LDmatrices.sh
@@ -5,3 +5,5 @@ plink --bfile gasAcu.plink19.maxDecel --extract md_sig_snps.txt --r2 square --al
 plink --bfile gasAcu.plink19.time_HDvMG --extract time_hdvmg_sugg_sig_snps.txt --r2 square --allow-extra-chr --out time_hdvmg_ld_matrix
 plink --bfile gasAcu.plink19.time_maxDecelvMG --extract time_mdvmg_sugg_sig_snps.txt --r2 square --allow-extra-chr --out time_mdvmg_ld_matrix
 plink --bfile gasAcu.plink19.ttpg --extract ttpg_sig_snps.txt --r2 square --allow-extra-chr --out ttpg_ld_matrix
+
+plink --bfile gasAcu.plink19.ray --extract ray_sig_snps.txt --r2 square --allow-extra-chr --out ray_ld_matrix
