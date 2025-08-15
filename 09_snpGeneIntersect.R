fa279c59a651475bc8a1ce888de3c789989e5821
Sara J. Smith <ssmith6@mtroyal.ca>
2025-08-15 14:23:39 -0600

    Update 09_snpGeneIntersect.R

--- 09_snpGeneIntersect.R
+++ 09_snpGeneIntersect.R
@@ -1,14 +1,15 @@
-library(tidyverse)
+#### Create gene lists for associated SNPs
 
-genes <- read.table("geneAssoc/gasAcu.genes.clean.bed", col.names = c("chr", "start", "end", "genes", "snp", "pheno")) %>%
+genes <- read.table("geneAssoc/gasAcu.genes.clean.bed", sep = '\t', 
+                    col.names = c("chr", "start", "end", "genes", "snp", "pheno")) %>%
   arrange(pheno, chr, start, end) %>%
   select(snp, pheno, genes) 
 
-gene_split <- as_tibble(str_split(genes$genes, ",", n = Inf, simplify = T))
+gene_split <- as_tibble(str_split(genes$genes, " ", n = Inf, simplify = T))
 
 gene_clean <- genes %>%
   bind_cols(., gene_split) %>%
-  filter(snp != "9:20267308") %>% # gets rid of one leftover LOC*
+  filter(snp != "12:16000501") %>% # gets rid of one leftover LOC*
   select(-c(genes))
 
 write_delim(gene_clean, "geneAssoc/gasAcu.final.cleanGenes.bed", delim = "\t")
@@ -16,6 +17,9 @@ write_delim(gene_clean, "geneAssoc/gasAcu.final.cleanGenes.txt", delim = "\t")
 write_delim(gene_clean, "geneAssoc/gasAcu.final.cleanGenes.csv", delim = ",")
 
 snpList <- gene_clean %>%
-  select(chr, end)
+  select(-c(snp, pheno)) %>%
+  pivot_longer(cols = everything(), values_to = "gene") %>%
+  filter(!is.na(gene), gene != "") %>%
+  distinct(gene)
 
-write_delim(snpList, "geneAssoc/snpList", delim = "\t")
+write_delim(snpList, "geneAssoc/snpList", delim = "\t", col_names = F)
