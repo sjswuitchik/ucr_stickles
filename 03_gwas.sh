a334d1a904afe7c7f0c9109d1b4cbd1e812569eb
Sara J. Smith <ssmith6@mtroyal.ca>
2025-08-15 14:01:30 -0600

    Update 03_gwas.sh

--- 03_gwas.sh
+++ 03_gwas.sh
@@ -1,9 +1,6 @@
-# create env for gemma gwas
-## NB: ended up not using GEMMA for GWAS but the bcf/vcftools still worked in this conda env
-conda create -n gwas -c esgf -c bioconda -c ostrokach -c conda-forge python=3.10 plink vcftools bcftools gemma libgfortran5 
-conda activate gwas
-
 # in /home/sjsmith/projects/def-sjsmith/sjsmith/stickles_ucr
+
+conda activate vcfFilt
 
 ## chromosomes need to be converted from alpahnumeric to numeric values. 
 # create chromosome map
@@ -26,7 +23,6 @@ vcftools --vcf gasAcu.chrRename.final.recode.vcf --remove-indv dedup/BAM_19.dedu
 ##### use the gasAcu.chrRename.noFails.recode.vcf for the continuous variable GWAS
 
 conda deactivate 
-conda create -n plink2 -c bioconda plink2
 conda activate plink2
 
 #success-failure
@@ -41,14 +37,16 @@ do
   plink2 --vcf gasAcu.chrRename.noFails.recode.vcf --make-pgen --allow-extra-chr --set-all-var-ids @:# --snps-only --hwe 0.05 --pheno phenos.cont.plink2.tsv --pheno-name $file --out gasAcu.plink.$file
 done < "cont.phenos"
 
-plink2 --pfile gasAcu.plink.sf --allow-extra-chr --glm --adjust --out gasAcu.plink
-mv gasAcu.plink.log gasAcu.plink.sf.log
+# convert plink 2.0 to plink 1.9 for LD analyses
 
-while IFS= read -r file
-do
-  plink2 --pfile gasAcu.plink.$file --allow-extra-chr --glm --adjust --out gasAcu.plink
-  mv gasAcu.plink.log gasAcu.plink.$file.log
-done < "cont.phenos"
+while read file
+do 
+  plink2 --pfile gasAcu.plink.$file --make-bed --allow-extra-chr --out gasAcu.plink19.$file
+done < cont.phenos
+
+plink2 --pfile gasAcu.plink.sf --make-bed --allow-extra-chr --out gasAcu.plink19.sf
+
+plink2 --pfile gasAcu.plink.ppdmg --make-bed --allow-extra-chr --out gasAcu.plink19.ppdmg
 
 mkdir gwas_results
 
@@ -59,14 +57,4 @@ done < "cont.phenos"
 
 mv gasAcu.plink.sf* gwas_results/
 
-# convert plink 2.0 to plink 1.9 for LD analyses
-cp cont.phenos gwas_results/
-
-while read file
-do 
-  plink2 --pfile gasAcu.plink.$file --make-bed --allow-extra-chr --out gasAcu.plink19.$file
-done < cont.phenos
-
-plink2 --pfile gasAcu.plink.sf --make-bed --allow-extra-chr --out gasAcu.plink19.sf
-
-plink2 --pfile gasAcu.plink.ppdmg --make-bed --allow-extra-chr --out gasAcu.plink19.ppdmg
+mv gasAcu.plink.ppdmg* gwas_results/
